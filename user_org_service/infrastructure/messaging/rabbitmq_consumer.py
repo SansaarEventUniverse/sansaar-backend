20c7c52c9d385d7879a95c540a07ad4d750812f8
kamaldhitalofficial <kamaldhitalofficial@gmail.com>
2025-09-25 13:47:44 +0545

    feat(infrastructure): add profile management infrastructure with integration tests

--- user_org_service/infrastructure/messaging/rabbitmq_consumer.py
+++ user_org_service/infrastructure/messaging/rabbitmq_consumer.py
@@ -3,6 +3,8 @@ import json
 import pika
 from django.conf import settings
 
+from infrastructure.messaging.user_registered_event_handler import UserRegisteredEventHandler
+
 
 class RabbitMQConsumer:
     def __init__(self):
@@ -19,11 +21,12 @@ class RabbitMQConsumer:
         )
         self.channel = self.connection.channel()
         self.channel.queue_declare(queue='user_registered', durable=True)
+        self.handler = UserRegisteredEventHandler()
 
     def callback(self, ch, method, properties, body):
         data = json.loads(body)
         print(f"Received UserRegistered event: {data}")
-        # TODO: Handle user registration event
+        self.handler.handle(data)
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
     def start_consuming(self):
