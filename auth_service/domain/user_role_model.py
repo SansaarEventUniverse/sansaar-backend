09e36735464a02855ccaa6face5ed6b336b6b4e0
kamaldhitalofficial <kamaldhitalofficial@gmail.com>
2025-10-07 04:14:27 +0545

    feat: add default USER role on registration

---
+++ auth_service/domain/user_role_model.py
@@ -0,0 +1,11 @@
+from django.db import models
+from .user_model import User
+
+
+class UserRole(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_role')
+    role = models.CharField(max_length=50, default='USER')
+    assigned_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        db_table = 'user_roles'
