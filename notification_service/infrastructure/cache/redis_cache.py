3709f32c8abb18ac38f2bbc64effe003fb4728b1
kamaldhitalofficial <kamaldhitalofficial@gmail.com>
2026-01-22 21:28:02 +0545

    feat: setup notification_service infrastructure

---
+++ notification_service/infrastructure/cache/redis_cache.py
@@ -0,0 +1,15 @@
+import redis
+from django.conf import settings
+
+class RedisCache:
+    def __init__(self):
+        self.client = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB, decode_responses=True)
+
+    def get(self, key):
+        return self.client.get(key)
+
+    def set(self, key, value, ttl=3600):
+        self.client.setex(key, ttl, value)
+
+    def delete(self, key):
+        self.client.delete(key)
